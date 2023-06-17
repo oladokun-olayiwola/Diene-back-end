d204bad1446243c139599e94878a042a85889de4
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2023-06-17 18:30:09 +0100

    Delete dist directory

--- dist/errors/customAPIError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-class customAPIError extends mongoose_1.Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = customAPIError;
--- dist/extras/createUserToken.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createTokenUser = (user) => {
-    return { name: user.name, _id: user._id };
-};
-exports.default = createTokenUser;
